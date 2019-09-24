edd04761c655bf6c640695fc31c5942663bf7b76
Rub21 <rub2106@gmail.com>
2019-09-24 09:13:20 -0500

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-export default class componentName extends Component {
-  render() {
-    return (
-      <header>
-        <div className="header-container">
-          <ul className="header_navigation" role="navigation">
-            <li className="header_navigation_collased_icon"></li>
-            <li className="header_navigation_title">
-              <h1>CVAT images validator</h1>
-            </li>
-          </ul>
-        </div>
-      </header>
-    );
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    background: '#cf3f02'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
   }
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" style={{ backgroundColor: '#cf3f02' }}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            CVAT images validator
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
