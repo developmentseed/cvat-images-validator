a1887969d145aff10974cfa05b7e93db07800110
Rub21 <rub2106@gmail.com>
2020-02-05 13:35:01 -0500

    Update

--- src/components/Copy.js
+++ src/components/Copy.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { withStyles } from '@material-ui/core/styles';
-
 import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
 
 const styles = {
   input: {
@@ -31,11 +29,6 @@ class Copy extends React.Component {
             value={this.props.imgPath}
           />
         </CopyToClipboard>
-        {/* {this.state.copied ? (
-          <Typography paragraph color="textSecondary">
-            Copied.
-          </Typography>
-        ) : null} */}
       </div>
     );
   }
