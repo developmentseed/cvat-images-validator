61b8974ae7fb8db75867a3ac6462a549d16323d9
yunica <fmjg21@gmail.com>
2021-02-01 18:42:46 -0500

    redux log

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
-const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+
 export default store;
